c8dcbeaaa2daf99ac6339f0d0db42142e141ff35
sowais <owais.chishti@blutechconsulting.com>
2020-05-10 02:47:22 +0500

    little edits in nav side bar

--- src/data/sidebar-nav-items.js
+++ src/data/sidebar-nav-items.js
@@ -21,20 +21,20 @@ export default function() {
       htmlBefore: '<i class="material-icons">view_module</i>',
       to: "/components-overview",
     },
-    {
-      title: "Tables",
-      htmlBefore: '<i class="material-icons">table_chart</i>',
-      to: "/tables",
-    },
-    {
-      title: "User Profile",
-      htmlBefore: '<i class="material-icons">person</i>',
-      to: "/user-profile-lite",
-    },
-    {
-      title: "Errors",
-      htmlBefore: '<i class="material-icons">error</i>',
-      to: "/errors",
-    }
+    // {
+    //   title: "Tables",
+    //   htmlBefore: '<i class="material-icons">table_chart</i>',
+    //   to: "/tables",
+    // },
+    // {
+    //   title: "User Profile",
+    //   htmlBefore: '<i class="material-icons">person</i>',
+    //   to: "/user-profile-lite",
+    // },
+    // {
+    //   title: "Errors",
+    //   htmlBefore: '<i class="material-icons">error</i>',
+    //   to: "/errors",
+    // }
   ];
 }
