0adb50cbf79a6fc7d1d3888ed4fd20a6f463a5c5
sowais <owais.chishti@blutechconsulting.com>
2020-05-17 23:23:07 +0500

    users profile graphs APIs integrated

--- src/views/UserProfileLite.js
+++ src/views/UserProfileLite.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Chart from "../utils/chart";
 import PropTypes from "prop-types";
+import axios from 'axios';
 import {
   Container,
   Row,
@@ -22,13 +23,13 @@ class UserProfileLite extends React.Component{
     super(props);
     this.state={
       title: "Users Statistics",
-      GenderData: {
+      GenderData: { 
         datasets: [
           {
             hoverBorderColor: "#ffffff",
             label : "Male/Female Count",
             // order: 4,
-            data: [24, 22, 0],
+            data: [],
             backgroundColor: [
               "rgba(0,123,255,0.9)",
               "rgba(0,123,255,0.5)"            ]
@@ -56,7 +57,7 @@ class UserProfileLite extends React.Component{
             ]
           }
         ],
-        labels: ["(0-2 Years)", "(4-6 Years)", "(8-12 Years)",  "(15-20 Years)", "(25-32 Years)", "(38-43 Years)", "(48-53 Years)", "(60-100 Years)",]
+        labels: ["(0-2 Years)", "(4-6 Years)", "(8-12 Years)",  "(15-20 Years)", "(25-32 Years)", "(38-43 Years)", "(48-53 Years)", "(60-100 Years)"]
       },
       EmotionsData: {
         datasets: [
@@ -83,24 +84,110 @@ class UserProfileLite extends React.Component{
   }
 
   componentDidMount() {
-    const chartConfig = {
-      type: "bar",
-      data: this.state.GenderData,
-      
-    };
-    const chartConfig2 = {
-      type: "bar",
-      data: this.state.AgeData,
-      
-    };
-    const chartConfig3 = {
-      type: "bar",
-      data: this.state.EmotionsData,
-      
-    }
-    new Chart(this.canvasRef.current, chartConfig);
-    new Chart(this.canvasRef2.current, chartConfig2);
-    new Chart(this.canvasRef3.current, chartConfig3);
+    // http://192.168.8.105:5000/counter
+    axios.get('http://192.168.8.105:5000/counter')
+          .then(response => {
+              if (response.status === 200 && response != null) {
+                let GenderData_local= { 
+                  datasets: [
+                    {
+                      hoverBorderColor: "#ffffff",
+                      label : "Male/Female Count",
+                      // order: 4,
+                      data: [],
+                      backgroundColor: [
+                        "rgba(0,123,255,0.9)",
+                        "rgba(0,123,255,0.5)"            ]
+                    }
+                  ],
+                  labels: ["Male", "Female"]
+                }
+                let data_labels = ['Male', 'Female']
+                for(var i = 0; i < response.data.gender.length; i++){
+                  GenderData_local.datasets[0].data.push(response.data.gender[i][data_labels[i]]);
+                }
+                // GenderData_local.datasets[0].data.push(0);
+                this.setState({
+                  GenderData : GenderData_local
+                });
+                const chartConfig = {
+                  type: "bar",
+                  data: this.state.GenderData,
+                };
+              
+              new Chart(this.canvasRef.current, chartConfig);
+              // API call 2 for GENDER GRAPH
+              let data_labels2 = ["(0-2)", "(4-6)", "(8-12)",  "(15-20)", "(25-32)", "(38-43)", "(48-53)", "(60-100)"]
+              let AgeData_local= {
+                datasets: [
+                  {
+                    hoverBorderColor: "#ffffff",
+                    label : "Gender Count",
+                    // order: 4,
+                    data: [],
+                    backgroundColor: [
+                      "rgba(0,123,255,0.9)",
+                      "rgba(0,123,255,0.8)",
+                      "rgba(0,123,255,0.7)",
+                      "rgba(0,123,255,0.6)",
+                      "rgba(0,123,255,0.5)",
+                      "rgba(0,123,255,0.4)",
+                      "rgba(0,123,255,0.3)",
+                      "rgba(0,123,255,0.2)",
+                    ]
+                  }
+                ],
+                labels: ["(0-2 Years)", "(4-6 Years)", "(8-12 Years)",  "(15-20 Years)", "(25-32 Years)", "(38-43 Years)", "(48-53 Years)", "(60-100 Years)"]
+              }
+              for(var i = 0; i < response.data.age.length; i++){
+                AgeData_local.datasets[0].data.push(response.data.age[i][data_labels2[i]]);
+              }
+              this.setState({
+                AgeData : AgeData_local
+              })
+              const chartConfig2 = {
+                type: "bar",
+                data: this.state.AgeData,
+              };
+              new Chart(this.canvasRef2.current, chartConfig2);
+
+              // API CALL 3 for EMOTIONS GRAPH
+              let data_labels3 = ["Anger", "Happy", "Sad", "Neutral"];
+              let EmotionsData_local= {
+                datasets: [
+                  {
+                    hoverBorderColor: "#ffffff",
+                    label : "Customers Emotions Count",
+                    // order: 4,
+                    data: [],
+                    backgroundColor: [
+                      "rgba(0,0,255,0.9)",
+                      "rgba(0,255,0,0.9)",
+                      "rgba(204,255,51,0.9)",
+                      "rgba(255,0,0,0.9)",
+                    ]
+                  }
+                ],
+                labels: ["Anger", "Happy", "Sad", "Neutral"] 
+              }
+              for(var i = 0; i < response.data.emotion.length; i++){
+                EmotionsData_local.datasets[0].data.push(response.data.emotion[i][data_labels3[i]]);
+              }
+              console.log(EmotionsData_local);
+              this.setState({
+                EmotionsData : EmotionsData_local
+              })
+              const chartConfig3 = {
+                type: "bar",
+                data: this.state.EmotionsData,
+              };
+              new Chart(this.canvasRef3.current, chartConfig3);
+              }
+              else{
+                console.log("Problem");
+              }
+            }
+          );
   }
 render() {
   const { title } = this.state;
